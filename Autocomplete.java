29b0ed68175142bff2cac48a21e244fe629a98e0
GameGenesis <gamegenesisdev@gmail.com>
2022-09-22 22:20:37 -0400

    Get autocomplete context

--- Autocomplete.java
+++ Autocomplete.java
@@ -9,9 +9,18 @@ public class Autocomplete {
 
     public final static Path filePath = Path.of("data.txt");
     public static String data = "";
+    public static String prompt = "";
+    public static int contextWords = 2;
 
     public static void main(String[] args) {
         data = getData(filePath).toLowerCase();
+        prompt = Str.input("Enter text to autocomplete: ");
+        String promptWords[] = prompt.split(" ");
+        String context = promptWords[promptWords.length - contextWords];
+        for (int i = contextWords - 1; i > 0; i--) {
+            context = String.join(" ", context, promptWords[promptWords.length - i]);
+        }
+        context = (context + " ").toLowerCase();
     }
 
     public static String getData(Path filePath) {
